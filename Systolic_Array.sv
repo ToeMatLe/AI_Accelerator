68cde0c5ed657ff9773a24e283f015e0e910b497
TomLe1227 <2005thomasle@gmail.com>
2026-06-30 19:53:17 -0700

    rename to Systolic_Array

--- Systolic_Array.sv
+++ Systolic_Array.sv
@@ -1,8 +1,8 @@
 module Systoic_Array #(
     parameter DATA_SIZE = 8,   // Integer size
     parameter ACC_SIZE = 32,   // Accumulator size
-    parameter MATRIX_SIZE = 2   // Size of the systolic array (2x2) -> Can seperate into L x W later
-)(
+    parameter MATRIX_SIZE = 2,   // Size of the systolic array (2x2) -> Can seperate into L x W later
+    )(
     input logic clk,
     input logic rst_n,
 
@@ -12,8 +12,8 @@ module Systoic_Array #(
     output logic [ACC_SIZE-1:0] output_C [0:MATRIX_SIZE-1][0:MATRIX_SIZE-1] // Output Matrix C [0:MATRIX_SIZE-1][0:MATRIX_SIZE-1] (OUTPUT)
 );
 
-logic [DATA_SIZE-1:0] top_wire [0:MATRIX_SIZE+1][0:MATRIX_SIZE];  // Wiring mesh to shift right
-logic [DATA_SIZE-1:0] left_wire [0:MATRIX_SIZE][0:MATRIX_SIZE+1]; // Wiring mesh to shift down
+logic [DATA_SIZE-1:0] top_wire [0:MATRIX_SIZE][0:MATRIX_SIZE-1];  // Wiring mesh to shift right
+logic [DATA_SIZE-1:0] left_wire [0:MATRIX_SIZE-1][0:MATRIX_SIZE]; // Wiring mesh to shift down
 assign top_wire[0] = input_B; // Connect input_B to the top row of the systolic array
 for (genvar i = 0; i < MATRIX_SIZE; i++) begin
     assign left_wire[i][0] = input_A[i]; // Connect input_A to the left column of the systolic array
@@ -32,7 +32,7 @@ generate
 
                 .input_A(left_wire[i][j]),
                 .input_B(top_wire[i][j]),
-                
+
                 // Shifts in systolic array
                 .output_A(left_wire[i][j+1]),    
                 .output_B(top_wire[i+1][j]),
